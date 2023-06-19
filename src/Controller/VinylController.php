80c14419a948a747f9b11c2ea3f575a4d3755109
Arnaud GARING <arnaud.garing67@gmail.com>
2023-06-19 15:10:45 +0200

    formatting and adding browse route

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,11 +4,26 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
     #[Route('/')]
-    public function homepage(){
+    public function homepage()
+    {
         return new Response('Title: PB and Jams');
     }
+
+    #[Route('/browse/{slug}')]
+    function browse(string $slug = null)
+    {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+
+        return new Response($title);
+    }
+
 }
