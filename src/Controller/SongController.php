d8b2d2d92f9489eb2b1215353eab9e44e27e5dd7
Arnaud GARING <arnaud.garing67@gmail.com>
2023-06-19 18:10:31 +0200

    added logger interface & cleaned VinylController.php

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/song/{id<\d+>}', methods: 'GET')]
-    function getSong(int $id): Response
+    function getSong(int $id, LoggerInterface $logger): Response
     {
         // todo: query the database
         $song = [
@@ -18,6 +19,10 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
 
         return $this->json($song);
     }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
@@ -20,8 +19,6 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
-
-        dump($tracks);
 
         return $this->render('vynil/homepage.html.twig', [
             'title' => 'PB & Jams',
