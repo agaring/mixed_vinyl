fa2260e8d93399bc6cd25b020191cc1eec08ae13
Arnaud GARING <arnaud.garing67@gmail.com>
2023-06-29 07:06:18 +0200

    add sensio/framework-extra-bundle

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -33,10 +33,8 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show(int $id, VinylMixRepository $mixRepository)
+    public function show(VinylMix $mix)
     {
-        $mix = $mixRepository->find($id);
-
         return $this->render('mix/show.html.twig',[
             'mix' => $mix
         ]);
